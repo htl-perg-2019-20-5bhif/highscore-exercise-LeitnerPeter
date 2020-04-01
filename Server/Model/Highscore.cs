3d9a6408577b2f2ae0c581a69b07e573621dbec5
pedaleidna <pedaleidna33@gmail.com>
2020-04-01 17:02:10 +0200

    no Unit Test but SQL Server

---
+++ Server/Model/Highscore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Model
+{
+    public class Highscore
+    {
+        public int ID { get; set; }
+        public string Initials { get; set; }
+        public int Points { get; set; }
+    }
+}
