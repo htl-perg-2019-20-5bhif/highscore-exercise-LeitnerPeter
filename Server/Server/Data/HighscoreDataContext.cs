be85e5cb3e7b040e2db25fd080c31a14ccd38688
pedaleidna <pedaleidna33@gmail.com>
2020-04-01 15:20:24 +0200

    no Tests but SQL Server

---
+++ Server/Server/Data/HighscoreDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Data
+{
+    public class HighscoreDataContext : DbContext
+    {
+        public HighscoreDataContext(DbContextOptions<HighscoreDataContext> options) : base(options)
+        { }
+
+        public DbSet<Highscore> Highscores { get; set; }
+    }
+}
